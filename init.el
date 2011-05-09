a11e731d45e144bd1c2c223ea73a0946804b0c46
Jon-Michael Deldin <dev@jmdeldin.com>
2011-05-09 14:53:52 -0600

    Add shortcuts for moving between windows

--- init.el
+++ init.el
@@ -226,6 +226,11 @@
 ;; C-x C-k -- kill region (since we just unbound it with C-w)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 
+(global-set-key (kbd "M-<left>")  'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
+(global-set-key (kbd "M-<up>")    'windmove-up)
+(global-set-key (kbd "M-<down>")  'windmove-down)
+
 ;;
 ;;; Macros
 ;;
