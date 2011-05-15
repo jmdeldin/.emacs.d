67459eda2f35be1437502dd5bde8b2af13cc994a
Jon-Michael Deldin <dev@jmdeldin.com>
2011-05-15 00:31:33 -0600

    Add ruby-run-buffer function & keymap

--- site-lisp/_ruby.el
+++ site-lisp/_ruby.el
@@ -1,3 +1,9 @@
+(defun ruby-run-buffer ()
+  "Run the current Ruby script and switch focus back to the script."
+  (interactive)
+  (ruby-compilation-this-buffer)
+  (other-window -1))
+
 (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
@@ -16,7 +22,8 @@
             (autoload 'inf-ruby-keys "inf-ruby")
             (inf-ruby-keys)
             (autoload 'ri "ri")
-            (global-set-key (kbd "C-h r") 'ri)))
+            (global-set-key (kbd "C-h r") 'ri)
+            (global-set-key (kbd "C-c C-c") 'ruby-run-buffer)))
 
 (autoload 'rdoc-mode "rdoc-mode" "Major mode for rdoc files" t)
 (add-to-list 'auto-mode-alist '("\\.rdoc$" . rdoc-mode))
