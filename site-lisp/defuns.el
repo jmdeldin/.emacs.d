f26cbf39fedcd19bee31da5cedb1498c1a1e6c14
Jon-Michael Deldin <dev@jmdeldin.com>
2011-07-19 22:43:18 -0600

    Add swap-buffers-in-windows function

--- site-lisp/defuns.el
+++ site-lisp/defuns.el
@@ -61,6 +61,35 @@ From URL `http://kill-0.com/duplo/2010/03/04/emacs-ruby-mode-comment-keybinding/
    (if mark-active (list (region-beginning) (region-end))
      (list (line-beginning-position)
            (line-beginning-position 2)))))
+
+
+(defun swap-buffers-in-windows ()
+  "Swap buffers between two windows.
+
+By Chris Webber from URL `http://www.emacswiki.org/emacs/TransposeWindows'."
+  (interactive)
+  (if (and swapping-window
+           swapping-buffer)
+      (let ((this-buffer (current-buffer))
+            (this-window (selected-window)))
+        (if (and (window-live-p swapping-window)
+                 (buffer-live-p swapping-buffer))
+            (progn (switch-to-buffer swapping-buffer)
+                   (select-window swapping-window)
+                   (switch-to-buffer this-buffer)
+                   (select-window this-window)
+                   (message "Swapped buffers."))
+          (message "Old buffer/window killed.  Aborting."))
+        (setq swapping-buffer nil)
+        (setq swapping-window nil))
+    (progn
+      (setq swapping-buffer (current-buffer))
+      (setq swapping-window (selected-window))
+      (message "Buffer and window marked for swapping."))))
+
+(setq swapping-buffer nil)
+(setq swapping-window nil)
+
 (defun eshell/d (&optional dir)
   "Launches a dired instance in the current working directory or DIR."
   (if dir
