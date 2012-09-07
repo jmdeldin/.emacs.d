2b9ffc428a25cb07969caf3f497d44524a6b6fd7
Jon-Michael Deldin <dev@jmdeldin.com>
2012-09-07 14:43:11 -0600

    Update list of installed packages

--- pkgs.el
+++ pkgs.el
@@ -1,18 +1,28 @@
 (setq required-packages '(
   auctex
+  browse-kill-ring
+  evil
+  find-file-in-project
   findr
   flymake-ruby
+  graphviz-dot-mode
+  htmlize
   inf-ruby
   inflections
+  ir-black-theme
+  js3-mode
   jump
   magit
   markdown-mode
+  php+-mode
   rainbow-mode
   ruby-compilation
   ruby-mode
   scss-mode
+  slime
   smex
-  textmate
+  underwater-theme
+  undo-tree
   yaml-mode
   yari
 ))
