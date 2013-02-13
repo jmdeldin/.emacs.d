a5f6a12e6c5c1535c18f7cd8240462542b7210be
Jon-Michael Deldin <dev@jmdeldin.com>
2013-02-12 22:23:46 -0700

    Move package initialization and remove some dead code
    - Calling `(require 'org)` before `(package-initialize)` meant the often
      outdated version of org was being used for the whole session.
    
    - org-babel-load-file already checks the tangled file's timestamp, so
      the most-recent-p bit was redundant.

--- init.el
+++ init.el
@@ -9,19 +9,7 @@
 
 ;;; Code:
 
-(defun get-last-modifed (file)
-  "Returns the last modified date of a FILE."
-  (interactive)
-  (format-time-string "%Y-%m-%d %T"
-                      (nth 5 (file-attributes file))))
-
-(defun most-recent-p (file1 file2)
-  "Returns t if FILE1 was modified more recently than FILE2."
-  (string< (get-last-modifed file1) (get-last-modifed file2)))
-
-;; (if (most-recent-p "~/.emacs.d/jmdeldin.org" "~/.emacs.d/jmdeldin.el")
-(require 'org)
+(package-initialize)
 (org-babel-load-file (expand-file-name "jmdeldin.org" user-emacs-directory))
-  ;; (load-file "~/.emacs.d/jmdeldin.el"))
 
 ;;; init.el ends here
