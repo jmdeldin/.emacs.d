f1d3c25953b08261aaff7b34d5094cfd6100e69f
Jon-Michael Deldin <dev@jmdeldin.com>
2016-03-23 23:20:28 -0700

    ace-window is great for switching and swapping!
    swap with C-u M-s

--- site-lisp/defuns.el
+++ site-lisp/defuns.el
@@ -82,33 +82,6 @@ From URL `http://kill-0.com/duplo/2010/03/04/emacs-ruby-mode-comment-keybinding/
      (list (line-beginning-position)
            (line-beginning-position 2)))))
 
-(defun swap-buffers-in-windows ()
-  "Swap buffers between two windows.
-
-By Chris Webber from URL `http://www.emacswiki.org/emacs/TransposeWindows'."
-  (interactive)
-  (if (and swapping-window
-           swapping-buffer)
-      (let ((this-buffer (current-buffer))
-            (this-window (selected-window)))
-        (if (and (window-live-p swapping-window)
-                 (buffer-live-p swapping-buffer))
-            (progn (switch-to-buffer swapping-buffer)
-                   (select-window swapping-window)
-                   (switch-to-buffer this-buffer)
-                   (select-window this-window)
-                   (message "Swapped buffers."))
-          (message "Old buffer/window killed.  Aborting."))
-        (setq swapping-buffer nil)
-        (setq swapping-window nil))
-    (progn
-      (setq swapping-buffer (current-buffer))
-      (setq swapping-window (selected-window))
-      (message "Buffer and window marked for swapping."))))
-
-(setq swapping-buffer nil)
-(setq swapping-window nil)
-
 (defun filter (cond-p lst)
   "Delete elements from a LST not matching condition COND-P."
   (delq nil
