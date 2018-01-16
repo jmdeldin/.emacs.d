b37fd37dd1e39e1e1c22f64346d4c89559bceb6c
Jon-Michael Deldin <dev@jmdeldin.com>
2018-01-16 14:44:34 -0800

    Revert "use-package is now the only package we _have_ to install"
    This reverts commit 55a74418a47ec903c13aca41d446294acf29ca7f.

--- site-lisp/defuns.el
+++ site-lisp/defuns.el
@@ -10,6 +10,24 @@
   "Path to ~/.emacs.d/local/FILE."
   (concat user-emacs-directory "local/" file))
 
+(defun jm/packages-installed-p (pkg-list)
+  "Determine whether all packages declared in PKG-LIST are installed."
+  (= (length pkg-list)
+     (length (jm/filter 'package-installed-p pkg-list))))
+
+(defun jm/install-packages (pkg-list)
+  "Install all packages declared in PKG-LIST."
+  (package-refresh-contents)
+  (mapcar (lambda (pkg)
+	    (unless (package-installed-p pkg)
+	      (package-install pkg)))
+	  pkg-list))
+
+(defun jm/install-all-packages (pkgs)
+  "Avoid pulling down (M)ELPA if the packages are already installed."
+  (unless (jm/packages-installed-p pkgs)
+    (jm/install-packages pkgs)))
+
 (defun jm/reload-init ()
   "Reload `~/.emacs.d/init.el'"
   (interactive)
